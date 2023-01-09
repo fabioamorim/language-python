7bff28b4e2f01b4dddb81655d44e8860cf8cd133
Amorim Fabio SRC FIPD33 <fabio.amorim@zf.com>
2023-01-09 16:39:38 -0300

    Add logical file

---
+++ 5-logical.py
@@ -0,0 +1,20 @@
+"""
+if/else/elif
+
+"""
+
+number = 100
+
+if number < 100:
+    print("less than 100")
+else:
+    print("equal or more than 100")
+
+number > 500
+
+if number < 500:
+    print("less than 500")
+elif number > 500:
+    print("more than 500")
+else:
+    print("equal 500")
