2b871b2a5469fecb5b0be8e8f7c44d3e82261174
Fabio Amorim <batista.amorim@hotmail.com>
2023-01-10 11:31:46 -0300

    Add and/or/is/not examples

--- 5-logical.py
+++ 5-logical.py
@@ -1,6 +1,21 @@
 """
 if/else/elif
 
+and/or/not/is
+
+and 
+
+True and True - True
+True and False - False
+False and True - False
+False and False - False
+
+or 
+
+True or True - True
+True or False - True
+False or True - True
+False or False - False
 
 """
 
@@ -19,3 +34,21 @@ elif number > 500:
     print("more than 500")
 else:
     print("equal 500")
+
+#--------------------------------------
+
+enable = False 
+logged = False 
+
+if enable and logged:
+    print("Welcome user")
+else:
+    print("The account is disable")
+
+if not enable:
+    print("The account is disable")
+
+if not logged:
+    print("The user is not logged")
+
+print(enable is False) # Is enable False? True
