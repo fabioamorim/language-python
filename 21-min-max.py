1f25447352a08f8ed6e476a0a69d0d67a197b14e
Fabio Amorim <batista.amorim@hotmail.com>
2023-01-17 15:36:29 -0300

    Add min and max example

---
+++ 21-min-max.py
@@ -0,0 +1,19 @@
+"""
+Min and Max
+"""
+
+lista = list(range(10))
+
+print(min(lista))
+# 0
+
+print(max(lista))
+# 9
+
+foo = {'a': 100, 'b': 20, 'c': 0, 'd': 80}
+
+print(max(foo.values()))
+# 100
+
+print(max(foo.keys()))
+# d
