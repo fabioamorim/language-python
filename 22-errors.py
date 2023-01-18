d3ae291660daa652b22396418a48597a017dd388
Fabio Amorim <batista.amorim@hotmail.com>
2023-01-18 17:09:19 -0300

    Erros exameple in progress

---
+++ 22-errors.py
@@ -0,0 +1,18 @@
+"""
+Common errors
+
+- NameError
+- ZeroDivisionError: number/0
+- SyntaxError
+
+"""
+
+# printf("test")
+# NameError: name 'printf' is not defined
+
+# 1/0
+# ZeroDivisionError: division by zero
+
+# def funcao:
+    #print('Hello World')
+# SyntaxError: invalid syntax
