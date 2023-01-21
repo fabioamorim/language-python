4c51d87bb9e64445dd52fe38f2a7d864df5cd720
Fabio Amorim <batista.amorim@hotmail.com>
2023-01-21 11:47:30 -0300

    Add errors examples

--- 23-errors.py
+++ 23-errors.py
@@ -4,6 +4,7 @@ Common errors
 - NameError
 - ZeroDivisionError: number/0
 - SyntaxError
+- IndexError
 
 """
 
@@ -16,3 +17,29 @@ Common errors
 # def funcao:
     #print('Hello World')
 # SyntaxError: invalid syntax
+
+# lista = list(range(10))
+
+# print(lista[12])
+# IndexError: list index out of range
+
+"""
+try:
+    len(True)
+except TypeError as err:
+    print(f'The app generated error: {err}')
+"""
+#The app generated error: object of type 'bool' has no len()
+
+"""
+try:
+    10/0
+except NameError as err1:
+    print(err1)
+except ZeroDivisionError as err2:
+    print(err2)
+except SystemError as err3:
+    print(err3)
+"""
+# division by zero
+
