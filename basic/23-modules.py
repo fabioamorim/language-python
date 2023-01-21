bb6965ef156c3586a1a16e195779a26d58e69208
Fabio Amorim <batista.amorim@hotmail.com>
2023-01-21 16:37:20 -0300

    Add package example

---
+++ basic/23-modules.py
@@ -0,0 +1,11 @@
+from modules.calc import calc_sum, calc_mult
+
+result = calc_sum(100, 200)
+
+print(result)
+#300
+
+result = calc_mult(20, 3)
+
+print(result)
+# 60
---
+++ basic/modules/calc.py
@@ -0,0 +1,14 @@
+
+pi = 3.1456
+
+def calc_sum(n1, n2):
+    return n1 + n2
+
+def calc_sub(n1, n2):
+    return n1 - n2
+
+def calc_mult(n1, n2):
+    return n1 * n2
+
+def calc_div(n1, n2):
+    return n1 / n2
