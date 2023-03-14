b384825565d45a68bf6c2ade6ccfe8131d3c03ae
Fabio Amorim <batista.amorim@hotmail.com>
2023-03-14 06:55:55 -0300

    update

---
+++ basic/30-csv.py
@@ -0,0 +1,20 @@
+from csv import reader, DictReader 
+path = './files/lutadores.csv'
+
+with open(path, encoding='utf-8') as arquivo:
+
+    data = reader(arquivo)
+    #data.__next__()
+    next(data)
+    for row in data:
+        print(row)
+
+print('\n')
+
+with open(path, encoding='utf-8') as arquivo:
+
+    data = DictReader(arquivo)
+
+    for row in data:
+        #print(row)
+        print(f'Nome: {row["Nome"]}')
